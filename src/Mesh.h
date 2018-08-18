0dc3c54775b4a25509feb355bd10368138d8bbdd
Ryohei Ikegami <iofg2100@gmail.com>
2018-08-18 16:16:54 +0900

    Declarations

--- src/Mesh.h
+++ src/Mesh.h
@@ -1,7 +1,18 @@
 #pragma once
 #include <QOpenGLExtraFunctions>
 
-class Mesh : protected QOpenGLExtraFunctions {
+class Mesh final : protected QOpenGLExtraFunctions {
 public:
     Mesh();
+    ~Mesh();
+
+    void draw();
+    virtual void setIndices(const std::vector<std::array<uint16_t, 3>>& indices);
+    virtual void setVertices(const std::vector<glm::vec2>& positions, const std::vector<glm::vec2>& texCoords);
+
+private:
+    GLuint _vertexArray = 0;
+    GLuint _vertexBuffer = 0;
+    GLuint _indexBuffer = 0;
+    int _triangleCount = 0;
 };
