9c93aa960fe6c73e585932c8182bbe1f80af4ae8
Ryohei Ikegami <iofg2100@gmail.com>
2018-08-18 16:37:34 +0900

    Add stub

--- src/Shader.cpp
+++ src/Shader.cpp
@@ -1,5 +1,45 @@
 #include "Shader.h"
 
-Shader::Shader() {
+Shader::Shader(const QString &vertexShader, const QString &fragmentShader) {
     initializeOpenGLFunctions();
+}
+
+Shader::~Shader() {
+
+}
+
+void Shader::bind() {
+
+}
+
+void Shader::unbind() {
+
+}
+
+void Shader::setUniform(const char *name, float value) {
+
+}
+
+void Shader::setUniform(const char *name, glm::vec2 value) {
+
+}
+
+void Shader::setUniform(const char *name, glm::vec3 value) {
+
+}
+
+void Shader::setUniform(const char *name, glm::vec4 value) {
+
+}
+
+void Shader::setUniform(const char *name, glm::mat2 value) {
+
+}
+
+void Shader::setUniform(const char *name, glm::mat3 value) {
+
+}
+
+void Shader::setUniform(const char *name, glm::mat4 value) {
+
 }
