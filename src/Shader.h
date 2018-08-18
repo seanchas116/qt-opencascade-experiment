ebeba26867f00cb3bef77f7ff4c7697d58752ad0
Ryohei Ikegami <iofg2100@gmail.com>
2018-08-18 16:35:55 +0900

    Declaration

--- src/Shader.h
+++ src/Shader.h
@@ -1,7 +1,20 @@
 #pragma once
 #include <QOpenGLExtraFunctions>
+#include <glm/glm.hpp>
 
-class Shader : protected QOpenGLExtraFunctions {
+class Shader final : protected QOpenGLExtraFunctions {
 public:
-    Shader();
+    Shader(const QString& vertexShader, const QString& fragmentShader);
+    ~Shader();
+
+    void bind();
+    void unbind();
+
+    void setUniform(const char* name, float value);
+    void setUniform(const char* name, glm::vec2 value);
+    void setUniform(const char* name, glm::vec3 value);
+    void setUniform(const char* name, glm::vec4 value);
+    void setUniform(const char* name, glm::mat2 value);
+    void setUniform(const char* name, glm::mat3 value);
+    void setUniform(const char* name, glm::mat4 value);
 };
