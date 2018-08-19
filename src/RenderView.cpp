c05929e4429314e5465e8f56b65d1bf583bab6d3
Ryohei Ikegami <iofg2100@gmail.com>
2018-08-19 16:39:32 +0900

    Draw triangle

--- src/RenderView.cpp
+++ src/RenderView.cpp
@@ -1,6 +1,7 @@
 #include "RenderView.h"
 #include "Shader.h"
 #include "Mesh.h"
+#include <array>
 
 RenderView::RenderView(QWidget* parent) : QOpenGLWidget(parent) {
 }
@@ -9,14 +10,28 @@ void RenderView::initializeGL() {
     initializeOpenGLFunctions();
 
     _shader = Shader::fromFiles(":/Default.vert", ":/Default.frag");
+    _shader->setUniform("MVP", glm::mat4(1));
     _mesh = std::make_shared<Mesh>();
+
+    std::vector<Vertex> vertices = {
+        {{0, -1, 0}, {0, 0}, {0, 0, 1}},
+        {{-1, 1, 0}, {0, 0}, {0, 0, 1}},
+        {{1, 1, 0}, {0, 0}, {0, 0, 1}},
+    };
+    std::vector<std::array<uint16_t, 3>> triangles = {
+        {{0, 1, 2}}
+    };
+    _mesh->setTriangles(triangles);
+    _mesh->setVertices(vertices);
 }
 
 void RenderView::resizeGL(int w, int h) {
     glViewport(0, 0, w, h);
-    glClearColor(0.6, 0.6, 0.6, 1);
-    glClear(GL_COLOR_BUFFER_BIT);
 }
 
 void RenderView::paintGL() {
+    glClearColor(0.6, 0.6, 0.6, 1);
+    glClear(GL_COLOR_BUFFER_BIT);
+    _shader->bind();
+    _mesh->draw();
 }
