710c85ec38e32bf250bfec4c35c2dc951d14b0a2
Ryohei Ikegami <iofg2100@gmail.com>
2018-08-19 16:32:24 +0900

    Use core profile

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,22 @@
 #include <QApplication>
 #include <QWidget>
+#include <QSurfaceFormat>
 #include "RenderView.h"
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
+
+    QSurfaceFormat glFormat;
+
+    // Request OpenGL 3.3 compatibility or OpenGL ES 3.0.
+    if (QOpenGLContext::openGLModuleType() == QOpenGLContext::LibGL) {
+        glFormat.setVersion(3, 3);
+        glFormat.setProfile(QSurfaceFormat::CoreProfile);
+    } else {
+        glFormat.setVersion(3, 0);
+    }
+
+    QSurfaceFormat::setDefaultFormat(glFormat);
 
     RenderView w;
     w.show();
